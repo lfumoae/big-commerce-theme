aecfe8754f9d1d3612fe7d072eed1f7bdb743a6f
unknown <gkotorov@gmail.com>
2020-12-15 18:34:19 +0200

    Add hawksearch config

--- assets/js/hawksearch/vue/index.js
+++ assets/js/hawksearch/vue/index.js
@@ -1,18 +1,17 @@
 import HawksearchVue from '@hawksearch/vue';
+import hawkConfig from './hawkConfig.json';
 
 export default function () {
     var components = document.querySelectorAll('[data-hawksearch-component="results"]');
 
     if (components) {
         components.forEach(component => {
-            const config = {
-                clientGuid: component.getAttribute('data-hawksearch-client-guid'),
-                apiUrl: component.getAttribute('data-hawksearch-search-api'),
-                indexName: component.getAttribute('data-hawksearch-index-name'),
-                searchConfig: {
-                    scrollUpOnRefresh: false
-                }
-            };
+            var config = hawkConfig;
+            config.clientGuid = component.getAttribute('data-hawksearch-client-guid');
+            config.indexName = component.getAttribute('data-hawksearch-index-name');
+            config.apiUrl = component.getAttribute('data-hawksearch-search-api') || hawkConfig.apiUrl;
+            config.dashboardUrl = component.getAttribute('data-hawksearch-dashboard-url') || hawkConfig.dashboardUrl;
+            config.websiteUrl = component.getAttribute('data-hawksearch-website-url') || hawkConfig.websiteUrl;
 
             var widget = HawksearchVue.createWidget(component, { config });
             HawksearchVue.initialSearch(widget);
